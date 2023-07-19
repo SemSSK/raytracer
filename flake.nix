d20172020b15c4726c7a355a2daa623941274aa9
semssk <ssksem015@gmail.com>
2023-07-19 01:28:51 +0100

    Cleaned up some files

--- flake.nix
+++ flake.nix
@@ -27,7 +27,9 @@
         devShell = with pkgs; mkShell {
           buildInputs = [
             pkg-config
-            rust-bin.stable.latest.default
+            rust-bin.stable.latest.default.override {
+              targets = ["wasm32-unknown-unknown" "x86_64-unknown-linux-gnu"];
+            }
             bacon
             udev
             rust-analyzer
